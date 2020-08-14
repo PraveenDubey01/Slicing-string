37d659a13d440e831954c180e178ab74c8e01fc9
Praveen Dubey <69203092+PraveenDubey01@users.noreply.github.com>
2020-08-14 18:35:38 +0530

    Add files via upload
    trim the string in desired way

---
+++ slicing.py
@@ -0,0 +1,8 @@
+x = "water"
+print(x[2])     #only print element of index inserted(0,1,2,3,4)
+x=x.replace('a' , 'l')   #replaces a with l
+print(x)
+print(x[2:5])     #splitting\removing 2 elements from string of 5 words as per exmp.
+x=x[4:5]          #splitting\removing 4 elements from string of 5 words as per exmp.
+print(x)
+print(x[-1])      # -ve indexing and splitting.
